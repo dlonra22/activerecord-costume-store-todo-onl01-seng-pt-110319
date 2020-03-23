e8c2881e8131ae10e9595287144fc507f6a1d4e9
arnie b <arnold.bwaila@gmail.com>
2020-03-23 14:30:46 +0000

    Automatically backed up by Learn

--- db/migrate/004_change_column_names.rb
+++ db/migrate/004_change_column_names.rb
@@ -1,6 +1,6 @@
 class ChangeColumnNames < ActiveRecord::Migration[5.1]
 
   def change
-    rename_column :students, :birthdate, :datetime
+    rename_column :haunted_house, :birthdate, :datetime
   end
 end
