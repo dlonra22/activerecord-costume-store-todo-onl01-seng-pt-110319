fe09198ee2a948f3ae61f2f92b42dd477c10a891
arnie b <arnold.bwaila@gmail.com>
2020-03-23 14:32:56 +0000

    Automatically backed up by Learn

--- db/migrate/004_change_column_names.rb
+++ db/migrate/004_change_column_names.rb
@@ -2,6 +2,6 @@ class ChangeColumnNames < ActiveRecord::Migration[5.1]
 
   def change
     rename_column :haunted_houses, :long_desciption, :description
-    rename_column :costume_stores, :number_of_employees, :numb_of_employees
+    rename_column :costume_stores, :number_of_employees, :num_of_employees
   end
 end
