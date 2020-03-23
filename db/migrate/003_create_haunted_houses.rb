dd1c9e2f5046f4215103d8f5e881797c66ec9d6b
arnie b <arnold.bwaila@gmail.com>
2020-03-23 14:30:28 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -13,7 +13,7 @@ class CreateHauntedHouses < ActiveRecord::Migration[5.2]
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.string :description
+      t.string :long_description
     end
   end
 end
